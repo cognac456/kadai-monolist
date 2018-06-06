cd02c1fdaff3a3e11667aeb096d749cd801a2926
cognac <femt456@gmail.com>
2018-06-06 11:57:53 +0000

    want

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root to: "toppages#index"
+  get "signup", to: "users#new"
+  resources :users, only: [:show, :new, :create]
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  resources :items, only: [:new, :show]
+  resources :ownerships, only: [:create, :destroy]
+  
+  get "rankings/want", to: "rankings#want"
+  
+end
